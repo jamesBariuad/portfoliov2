4af17c457966b6c87563ed52d28da26ffde8c39b
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2023-12-28 18:04:27 +0800

    about me and my work sections

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-
-
 const toggleMobileNav = ()=>{
     const mobileNav = document.getElementById("mobile-nav");
+    const hamburgerMenu = document.getElementById("hamburger-menu")
     if (mobileNav.style.display === "none") {
         mobileNav.style.display = "block";
+        hamburgerMenu.style.display = "none";
       } else {
         mobileNav.style.display = "none";
+        hamburgerMenu.style.display = "block";
       }
 }
