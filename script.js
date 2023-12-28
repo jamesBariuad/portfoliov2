713f1a9984810429885a1211988ad198ebe35dce
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2023-12-28 10:48:18 +0800

    mobile nav bar

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const toggleMobileNav = ()=>{
+    const mobileNav = document.getElementById("mobile-nav");
+    if (mobileNav.style.display === "none") {
+        mobileNav.style.display = "block";
+      } else {
+        mobileNav.style.display = "none";
+      }
+}
