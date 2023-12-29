2159dde6ee77733ef58f7d0c612271d1c1c3cd62
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2023-12-29 10:49:33 +0800

    added href links on navbar menu items

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const toggleMobileNav = ()=>{
-    const mobileNav = document.getElementById("mobile-nav");
-    const hamburgerMenu = document.getElementById("hamburger-menu")
-    if (mobileNav.style.display === "none") {
-        mobileNav.style.display = "block";
-        hamburgerMenu.style.display = "none";
-      } else {
-        mobileNav.style.display = "none";
-        hamburgerMenu.style.display = "block";
-      }
-}
+const toggleMobileNav = () => {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "block";
+    hamburgerMenu.style.display = "none";
+    document.body.style.overflow = "hidden";
+  } else {
+    mobileMenu.style.display = "none";
+    hamburgerMenu.style.display = "block";
+    document.body.style.overflow = "auto";
+  }
+};
