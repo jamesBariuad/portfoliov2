3f0493a1a8c82b8ba7aade4a713e2132c2ddb322
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-01-02 08:17:19 +0800

    added fontstyles

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        mainText: "Montserrat",
-        subText:"Merriweather",
+        mainText: ["Montserrat"],
+        subText:["Merriweather"],
       }
     },
   },
